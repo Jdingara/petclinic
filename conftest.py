a373f604cb58cebda1a7bd75477b3878ac761f3a
Sasi Kumar <sasiamudha6165@gmail.com>
2026-05-30 13:40:19 +0530

    fix: remove hardcoded secrets from CI, use environment variables

--- conftest.py
+++ conftest.py
@@ -1,6 +1,3 @@
-import django
-from django.conf import settings
-
-
-def pytest_configure(config):
-    settings.DJANGO_SETTINGS_MODULE = "config.settings"
+import os
+os.environ.setdefault('DJANGO_SECRET_KEY', 'test-only-not-for-production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
